17ab931f55345e527f478a255c514d2422245291
Robert Hughes <robert.hughes@bt.com>
2018-12-31 10:40:02 +0000

    Initial working version

--- book/convert.py
+++ book/convert.py
@@ -3,3 +3,14 @@ from datetime import timedelta
 
 def time_to_pages(completed: timedelta, overall: timedelta, pages: int) -> float:
     return (completed / overall) * pages
+
+
+def str_to_time(time: str) -> timedelta:
+    hours, minutes = map(lambda x: float(x), time.split(':'))
+    return timedelta(hours=hours, minutes=minutes)
+
+
+def run(completed: str, overall: str, pages: int):
+    completed_td = str_to_time(time=completed)
+    overall_td = str_to_time(time=overall)
+    return time_to_pages(completed_td, overall_td, pages)
--- tests/test_book.py
+++ tests/test_book.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from book import convert
+from book.__main__ import parse_time
 
 
 def test_convert_time():
@@ -7,3 +8,7 @@ def test_convert_time():
     overall = timedelta(hours=3)
     result = convert.time_to_pages(completed, overall, pages=100)
     assert result == 50
+
+
+def test_parse_time():
+    assert parse_time(time='1:30') == timedelta(hours=1, minutes=30)
